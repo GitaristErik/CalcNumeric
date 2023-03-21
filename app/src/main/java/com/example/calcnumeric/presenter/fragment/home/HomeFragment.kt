772cd3c17c50659983aec26846e7a7cbd876e89e
Erik <Gitarist.Erik@gmail.com>
2023-03-21 23:22:54 +0200

    Add home calculator xml view

--- app/src/main/java/com/example/calcnumeric/presenter/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/calcnumeric/presenter/fragment/home/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.calcnumeric.presenter.fragment.home
 
-import android.os.Bundle
+import android.animation.LayoutTransition
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.example.calcnumeric.R
 import com.example.calcnumeric.databinding.FragmentHomeBinding
 import com.example.calcnumeric.presenter.fragment.BaseViewModelFragment
 import com.example.calcnumeric.presenter.fragment.home.HomeViewModel.ViewData
+import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,15 +21,29 @@ class HomeFragment : BaseViewModelFragment<FragmentHomeBinding, ViewData, HomeVi
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun initializeView() {
-        log.d(" ")
+        initSwitchMenu()
     }
 
     override fun render(data: ViewData) {
         log.d(" ")
+    }
+
+    private fun initSwitchMenu() {
+        binding.tableLayout.layoutTransition = LayoutTransition().apply {
+             disableTransitionType(LayoutTransition.DISAPPEARING)
+        }
+
+        binding.btnSwitchAdditional.setOnClickListener {
+            if (it is MaterialButton) {
+                if (binding.menuAdditional.isVisible) {
+                    binding.menuAdditional.visibility = View.GONE
+                    it.setIconResource(R.drawable.ic_arrow_up_24dp)
+                } else {
+                    binding.menuAdditional.visibility = View.VISIBLE
+                    it.setIconResource(R.drawable.ic_arrow_down_24dp)
+                }
+            }
+        }
     }
 }
